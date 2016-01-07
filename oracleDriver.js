31d9494c0f7faf9dfd16503d540a90bb1a26d549
Tim Macfarlane <timmacfarlane@gmail.com>
2016-01-07 13:45:32 +0100

    connection pooling for oracledb

--- oracleDriver.js
+++ oracleDriver.js
@@ -1,5 +1,6 @@
 var optionalRequire = require('./optionalRequire');
 var promisify = require('./promisify');
+var debug = require('debug')('sworm:oracle');
 
 module.exports = function () {
   var oracledb = optionalRequire('oracledb');
@@ -8,13 +9,7 @@ module.exports = function () {
     query: function (query, params, options) {
       var self = this;
 
-      var results = params
-        ? promisify(function (cb) {
-            self.connection.execute(replaceParameters(query, params), params, cb);
-          })
-        : promisify(function (cb) {
-            self.connection.execute(query, cb);
-          });
+      var results = this.execute(replaceParameters(query, params), params);
 
       if (options && options.statement) {
         return results;
@@ -25,11 +20,32 @@ module.exports = function () {
       }
     },
 
+    execute: function (query, params) {
+      var self = this;
+      debug(query, params);
+      return promisify(function (cb) {
+        return params
+          ? self.connection.execute(query, params, cb)
+          : self.connection.execute(query, cb);
+      });
+    },
+
     connect: function (config) {
       var self = this;
 
+      if (config.options) {
+        Object.keys(config.options).forEach(function (key) {
+          console.log('setting ' + key + ': ' + config.options[key]);
+          oracledb[key] = config.options[key];
+        });
+      }
+
       return promisify(function (cb) {
-        oracledb.getConnection(config.config, cb);
+        if (config.config.pool) {
+          config.config.pool.getConnection(cb);
+        } else {
+          oracledb.getConnection(config.config, cb);
+        }
       }).then(function (connection) {
         self.connection = connection;
       });
@@ -92,7 +108,7 @@ function formatRows(results) {
 }
 
 function replaceParameters(query, params) {
-  return query.replace(/@([a-z_0-9]+)\b/g, function (_, paramName) {
+  return query.replace(/@([a-z_0-9]+)\b/gi, function (_, paramName) {
     if (!params.hasOwnProperty(paramName)) {
       throw new Error ('no such parameter @' + paramName);
     }
