9562e4101718c9cac3f9939e57c5901ce500ce9f
Tim Macfarlane <timmacfarlane@gmail.com>
2016-04-08 17:03:01 +0200

    mssql close returns promise

--- mssqlDriver.js
+++ mssqlDriver.js
@@ -39,7 +39,8 @@ module.exports = function() {
     },
 
     close: function() {
-      return this.connection.close();
+      this.connection.close();
+      return Promise.resolve();
     }
   };
 };
