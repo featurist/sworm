805615bd05383fdaa76f7ff44b410a0e72cb5be2
Tim Macfarlane <timmacfarlane@gmail.com>
2016-04-08 17:09:10 +0200

    don't run oracle in travis

--- test/oracleSpec.js
+++ test/oracleSpec.js
@@ -1,107 +1,107 @@
-var dockerHostname = require('./dockerHostname');
-var describeDatabase = require('./describeDatabase');
-var sworm = require('..');
-var oracledb = require('oracledb');
-var expect = require('chai').expect;
+if (!process.env.TRAVIS) {
+  var dockerHostname = require('./dockerHostname');
+  var describeDatabase = require('./describeDatabase');
+  var sworm = require('..');
+  var oracledb = require('oracledb');
+  var expect = require('chai').expect;
 
-var database = {
-  createTables: function(db, tables) {
-    function createTable(name, id, sql, noAutoId) {
-      tables.push(name);
-      return db.query(
-        `BEGIN
-           EXECUTE IMMEDIATE 'DROP TABLE ${name}';
-         EXCEPTION WHEN OTHERS THEN
-           IF SQLCODE != -942 THEN
-             RAISE;
-           END IF;
-         END;`
-      ).then(function() {
+  var database = {
+    createTables: function(db, tables) {
+      function createTable(name, id, sql, noAutoId) {
+        tables.push(name);
         return db.query(
           `BEGIN
-             EXECUTE IMMEDIATE 'DROP SEQUENCE ${name}_seq';
+             EXECUTE IMMEDIATE 'DROP TABLE ${name}';
            EXCEPTION WHEN OTHERS THEN
-             IF SQLCODE != -2289 THEN
+             IF SQLCODE != -942 THEN
                RAISE;
              END IF;
            END;`
         ).then(function() {
-          if (!noAutoId) {
-            return db.query(`CREATE SEQUENCE ${name}_seq`);
-          }
-        }).then(function() {
-          return db.query(sql).then(function() {
+          return db.query(
+            `BEGIN
+               EXECUTE IMMEDIATE 'DROP SEQUENCE ${name}_seq';
+             EXCEPTION WHEN OTHERS THEN
+               IF SQLCODE != -2289 THEN
+                 RAISE;
+               END IF;
+             END;`
+          ).then(function() {
             if (!noAutoId) {
-              return db.query(
-                `create or replace trigger ${name}_id
-                   before insert on ${name}
-                   for each row
-                 begin
-                   select ${name}_seq.nextval into :new.${id} from dual;
-                 end;`
-              );
+              return db.query(`CREATE SEQUENCE ${name}_seq`);
             }
+          }).then(function() {
+            return db.query(sql).then(function() {
+              if (!noAutoId) {
+                return db.query(
+                  `create or replace trigger ${name}_id
+                     before insert on ${name}
+                     for each row
+                   begin
+                     select ${name}_seq.nextval into :new.${id} from dual;
+                   end;`
+                );
+              }
+            });
           });
         });
-      });
-    }
+      }
 
-    return createTable("people", "id",
-      `create table people (
-         id number primary key,
-         name varchar2(50) NOT NULL,
-         address_id number NULL
-       )`
-    ).then(function() {
-      return createTable("people_addresses", "address_id",
-        `create table people_addresses(
-           address_id int NOT NULL,
-           person_id int NOT NULL,
-           rating int NULL
-         )`,
-         true
-      );
-    }).then(function() {
-      return createTable("addresses", "id",
-        `create table addresses(
+      return createTable("people", "id",
+        `create table people (
            id number primary key,
-           address varchar2(50) NOT NULL
+           name varchar2(50) NOT NULL,
+           address_id number NULL
          )`
-      );
-    }).then(function() {
-      return createTable("people_weird_id", "weird_id",
-        `create table people_weird_id(
-           weird_id number primary key,
-           name varchar2(50) NULL,
-           address_weird_id int NULL
-        )`
-      );
-    }).then(function() {
-      return createTable("people_explicit_id", "id",
-        `create table people_explicit_id(
-           id int NOT NULL,
-           name varchar2(50) NOT NULL
-         )`
-      );
-    });
-  },
+      ).then(function() {
+        return createTable("people_addresses", "address_id",
+          `create table people_addresses(
+             address_id int NOT NULL,
+             person_id int NOT NULL,
+             rating int NULL
+           )`,
+           true
+        );
+      }).then(function() {
+        return createTable("addresses", "id",
+          `create table addresses(
+             id number primary key,
+             address varchar2(50) NOT NULL
+           )`
+        );
+      }).then(function() {
+        return createTable("people_weird_id", "weird_id",
+          `create table people_weird_id(
+             weird_id number primary key,
+             name varchar2(50) NULL,
+             address_weird_id int NULL
+          )`
+        );
+      }).then(function() {
+        return createTable("people_explicit_id", "id",
+          `create table people_explicit_id(
+             id int NOT NULL,
+             name varchar2(50) NOT NULL
+           )`
+        );
+      });
+    },
 
-  "true": 1,
-  "false": 0,
+    "true": 1,
+    "false": 0,
 
-  clean: function(records) {
-    return records;
-  },
+    clean: function(records) {
+      return records;
+    },
 
-  driverModuleName: "oracledb"
-};
+    driverModuleName: "oracledb"
+  };
 
-var config = {
-  driver: "oracle",
-  config: { user: "system", password: "oracle", connectString: `${dockerHostname}/XE` }
-};
+  var config = {
+    driver: "oracle",
+    config: { user: "system", password: "oracle", connectString: `${dockerHostname}/XE` }
+  };
 
-if (!process.env.TRAVIS) {
   describeDatabase("oracle", config, database, function () {
     describe('options', function () {
       it('can pass options to query', function () {
