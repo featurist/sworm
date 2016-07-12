875e6a8182908d4a537286c561e400a87aa3c56f
Tim Macfarlane <timmacfarlane@gmail.com>
2016-07-12 13:41:11 +0200

    removed old docker for mac domain name

--- test/dockerHostname.js
+++ test/dockerHostname.js
@@ -1,13 +1,8 @@
 var urlUtils = require('url');
-var os = require('os');
 
 if (process.env.DOCKER_HOST) {
   var url = urlUtils.parse(process.env.DOCKER_HOST);
   module.exports = url.hostname;
 } else {
-  if (/darwin/i.test(os.type())) {
-    module.exports = 'docker.local';
-  } else {
-    module.exports = 'localhost';
-  }
+  module.exports = 'localhost';
 }
